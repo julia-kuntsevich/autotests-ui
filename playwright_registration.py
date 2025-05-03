eb361cc4e492a5e79cf8f9129fa928f138eac417
Юлия Сидорова <uliasidorova@MacBook-Air-Ulia.local>
2025-05-03 20:48:51 +0300

    Практика с Playwright

---
+++ playwright_registration.py
@@ -0,0 +1,28 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
+
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
+
+    with page.expect_navigation():
+     page.click('button:has-text("Registration")')
+
+    dashboard_toolbar_title = page.get_by_test_id('dashboard-toolbar-title-text')
+    title_text = dashboard_toolbar_title.text_content()
+    expected_title = "Dashboard"
+
+    if expected_title == title_text:
+     print("Есть заголовок - Dashboard")
+    else:
+     print("Заголовок отсутствует")
+
