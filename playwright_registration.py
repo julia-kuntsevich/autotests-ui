ffc4aec74e99d865fadd06bc36a7187c6048aad7
Юлия Сидорова <uliasidorova@MacBook-Air-Ulia.local>
2025-05-04 14:25:02 +0300

    Практика по углубленной работе с Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -14,15 +14,21 @@ with sync_playwright() as playwright:
     password_input = page.get_by_test_id('registration-form-password-input').locator('input')
     password_input.fill('password')
 
-    with page.expect_navigation():
-     page.click('button:has-text("Registration")')
+    # with page.expect_navigation():
+    #  page.click('button:has-text("Registration")')
+    #
+    # dashboard_toolbar_title = page.get_by_test_id('dashboard-toolbar-title-text')
+    # title_text = dashboard_toolbar_title.text_content()
+    # expected_title = "Dashboard"
+    #
+    # if expected_title == title_text:
+    #  print("Есть заголовок - Dashboard")
+    # else:
+    #  print("Заголовок отсутствует")
 
-    dashboard_toolbar_title = page.get_by_test_id('dashboard-toolbar-title-text')
-    title_text = dashboard_toolbar_title.text_content()
-    expected_title = "Dashboard"
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
 
-    if expected_title == title_text:
-     print("Есть заголовок - Dashboard")
-    else:
-     print("Заголовок отсутствует")
-
+    dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_title).to_be_visible()
+    expect(dashboard_title).to_have_text('Dashboard')
