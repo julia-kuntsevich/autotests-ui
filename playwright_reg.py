2429589bc0f6eabf20e0f0ae541adf22be88760c
Юлия Сидорова <uliasidorova@MacBook-Air-Ulia.local>
2025-05-08 22:59:00 +0300

    playwright_courses

--- playwright_reg.py
+++ playwright_reg.py
@@ -1,13 +1,11 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
 
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-
-    registration_button = page.get_by_test_id('registration-page-registration-button')
-    expect(registration_button).to_be_disabled()
 
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
     email_input.fill('user.name@gmail.com')
@@ -19,5 +17,15 @@ with sync_playwright() as playwright:
     password_input.fill('password')
 
     registration_button = page.get_by_test_id('registration-page-registration-button')
-    expect(registration_button).to_be_enabled()
- 
+    registration_button.click()
+
+    #сохраняем состояние браузера
+    context.storage_state(path='browser-state.json')
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+
