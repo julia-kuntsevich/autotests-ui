1254c028d26056b9a40fe55048137724b4a50544
Юлия Сидорова <uliasidorova@MacBook-Air-Ulia.local>
2025-05-17 07:56:27 +0300

    Практика работы с pytest фикстурами

--- tests/test_reg.py
+++ tests/test_reg.py
@@ -1,35 +1,23 @@
 import pytest
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Page
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+def test_successful_registration( chromium_page: Page):
+        chromium_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
         email_input.fill('user.name@gmail.com')
 
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
         username_input.fill('username')
 
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
         password_input.fill('password')
 
-        registration_button = page.get_by_test_id('registration-page-registration-button')
+        registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
         registration_button.click()
 
-        # сохраняем состояние браузера
-        context.storage_state(path='browser-state.json')
 
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state='browser-state.json')
-        page = context.new_page()
 
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
 
